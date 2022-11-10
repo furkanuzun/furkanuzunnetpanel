ba3ea1f600019530f01b0eff77c7639b006a8c83
Furkan Uzun <furkan@Furkan-MacBook-Air.local>
2022-11-10 19:01:14 +0300

    server.js edit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'furkanuzun'),
-      user: env('DATABASE_USERNAME', 'furkanuzun'),
+      database: env('DATABASE_NAME', 'httpdjbz_furkanuzun'),
+      user: env('DATABASE_USERNAME', 'httpdjbz_furkanuzun'),
       password: env('DATABASE_PASSWORD', 'database8563D'),
       ssl: env.bool('DATABASE_SSL', true),
     },
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,6 @@
 module.exports = ({ env }) => ({
-    url: env('https://panel.furkanuzun.net'),
-  });
+  url: env("https://panel.furkanuzun.net"),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
